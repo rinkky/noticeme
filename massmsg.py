5117e5b946c6e070bab0f92a01e84278a564372d
rinkkyjia@gmail.com <rinkkyjia@gmail.com>
2017-05-05 21:24:07 +0800

    change tabs to spaces

--- massmsg.py
+++ massmsg.py
@@ -13,17 +13,17 @@ from playstoredata import playstoredata
 import tokengen
 """
 url_token = (
-	"https://api.weixin.qq.com/cgi-bin/token"
-	"?grant_type=client_credential&appid={0}&secret={1}"
+    "https://api.weixin.qq.com/cgi-bin/token"
+    "?grant_type=client_credential&appid={0}&secret={1}"
 ).format(
-		wechatcfg.appid, 
-		wechatcfg.secret
+        wechatcfg.appid, 
+        wechatcfg.secret
 )
 
 r = requests.get(url_token)
 
 if r.status_code != 200:
-	sys.exit(0)
+    sys.exit(0)
 
 print(r.status_code)
 
@@ -32,34 +32,34 @@ access_token = data.get("access_token")
 """
 access_token = tokengen.get_token()
 url_sendmsg = (
-	"https://api.weixin.qq.com/cgi-bin/message/mass/sendall?access_token={0}"
+    "https://api.weixin.qq.com/cgi-bin/message/mass/sendall?access_token={0}"
 ).format(access_token)
 print(url_sendmsg)
 
 apps = playstoredata.get_notice_apps()
 if apps is None:
-	text_to_send = time.asctime() + "\nNo apps on sale"
+    text_to_send = time.asctime() + "\nNo apps on sale"
 else:
-	text_to_send = time.asctime() + "\nhere are the apps on sale:\n"
-	apps_detail = []
-	for app in apps:
-		apps_detail.append(
-			"{0}\n{1}\n{2}\n----\n".format(
-				app.uniq_name,
-				app.name,
-				app.price
-			)
-		)
-	text_to_send = text_to_send + "".join(apps_detail)
+    text_to_send = time.asctime() + "\nhere are the apps on sale:\n"
+    apps_detail = []
+    for app in apps:
+        apps_detail.append(
+            "{0}\n{1}\n{2}\n----\n".format(
+                app.uniq_name,
+                app.name,
+                app.price
+            )
+        )
+    text_to_send = text_to_send + "".join(apps_detail)
 print(text_to_send)
 
 data_to_send = {
-	"filter": {
-		"is_to_all": False,
-		"tag_id": 0
-	},
-	"msgtype": "text",
-	"text": {"content": text_to_send,}
+    "filter": {
+        "is_to_all": False,
+        "tag_id": 0
+    },
+    "msgtype": "text",
+    "text": {"content": text_to_send,}
 }
 data_to_send = json.dumps(data_to_send)
 headers = {"content-type": "application/json"}
@@ -70,4 +70,4 @@ print(r.status_code)
 print(r.text)
 errmsg = r.json()
 if errmsg.get("errcode") == 0:
-	playstoredata.clean_notice_apps()
+    playstoredata.clean_notice_apps()
--- tokengen.py
+++ tokengen.py
@@ -12,26 +12,26 @@ lastgettime = 0
 token = ""
 
 url_token = (
-	"https://api.weixin.qq.com/cgi-bin/token"
-	"?grant_type=client_credential&appid={0}&secret={1}"
+    "https://api.weixin.qq.com/cgi-bin/token"
+    "?grant_type=client_credential&appid={0}&secret={1}"
 ).format(
-		wechatcfg.appid, 
-		wechatcfg.secret
+        wechatcfg.appid, 
+        wechatcfg.secret
 )
 
 def get_token():
-	global token
-	global timeout
-	global lastgettime
-	if timeout + lastgettime < time.time():
-		lastgettime = time.time()
-		r = requests.get(url_token)
-		if r.status_code != 200:
-			return None
-		data = r.json()
-		timeout = data.get(WECHATKEY_EXPIRES_IN) - 100
-		token = data.get(WECHATKEY_ACCESS_TOKEN)
-	return token
+    global token
+    global timeout
+    global lastgettime
+    if timeout + lastgettime < time.time():
+        lastgettime = time.time()
+        r = requests.get(url_token)
+        if r.status_code != 200:
+            return None
+        data = r.json()
+        timeout = data.get(WECHATKEY_EXPIRES_IN) - 100
+        token = data.get(WECHATKEY_ACCESS_TOKEN)
+    return token
 
 if __name__ == '__main__':
-	print(get_token())
+    print(get_token())
